800caaed0a3386ae01dbc1963a31146aa4023ab7
Matthew Price <doggy1199@yahoo.com>
2013-02-05 08:47:09 -0700

    Setup HTree method draw

--- HTree.java
+++ HTree.java
@@ -12,7 +12,7 @@ class HTree {
 	private int x, y, length, depth;
 	private Graphics g;
 
-	public static final int MAX_DEPTH = 7;
+	public static final int MAX_DEPTH = 10;
 
 	/**
 	 * Constructor
@@ -27,5 +27,130 @@ class HTree {
 		this.length = length;
 		this.depth = 1;
 	}
+
+	/**
+	 * 
+	 * Getters and Setters
+	 * 
+	 */
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	// public void setDepth(int depth) {
+	// 	this.depth = depth;
+	// }
+
+	/**
+	 * incrementDepth
+	 *
+	 * 		increment the depth by one, unless the depth is greater than MAX_DEPTH, in which
+	 * 		case it should be set to one.
+	 */
+	public void incrementDepth() {
+		this.depth++;
+		if(this.depth==MAX_DEPTH) {
+			this.depth = 1;
+		}
+	}
+
+
+	/**
+	 * draw
+	 *
+	 * 		draw the htree to the screen
+	 *
+	 * 		this is an accessor method that calls the real private recursive method
+	 *
+	 * @param g - the Graphics obect associated with the window
+	 */
+	public void draw(Graphics g) {
+		this.g = g;
+		recursiveDraw(depth, x, y, length);
+	}
+
+
+	/**
+	 * recursiveDraw
+	 * 		actually draw the htree
+	 *
+	 * @param n - current depth of the htree
+	 * @param a, b - center point of the htree
+	 * @param s - length of a line at the current depth
+	 *
+	 * precondition - g must not be null
+	 */
+	private void recursiveDraw(int n, int a, int b, int s) {
+		//base case
+		if (n <= 0){
+			return;
+		}
+
+		//draw step
+		
+		//math to calculate new positions and sizes
+		int halfLength = s / 2;
+
+		//horizontal line
+		int x1 = a - halfLength;
+		int y1 = b;
+		int x2 = a + halfLength;
+		int y2 = b;
+		g.drawLine(x1, y1, x2, y2);
+
+		//vertical left
+		y1 = b - halfLength;
+		x2 = x1;
+		y2 = b + halfLength;
+		g.drawLine(x1, y1, x2, y2);
+
+		//reduction step
+		
+		//vertical right
+		x1 = a + halfLength;
+		x2 = x1;
+		g.drawLine(x1, y1, x2, y2);
+
+		//reduction step
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
