d268295669467b9dac7c3ba9c90bcc514899918d
Matthew Price <doggy1199@yahoo.com>
2013-02-05 09:09:02 -0700

    Started TTree

---
+++ TTree.java
@@ -0,0 +1,21 @@
+/**
+ * TTree
+ *
+ * 		recursively draws a TTree
+ */
+
+import java.awt.Graphics;
+
+class TTree {
+	
+	private int x, y, length, depth;
+	private Graphics g;
+
+	public static final int MAX_DEPTH = 7;
+
+	/**
+	 * constructor
+	 *
+	 * @param x - 
+	 */
+}
